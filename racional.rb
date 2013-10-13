cb20ed2ef59db4eb6b8d3f519d5de893f42c2d8f
José Alberto Pérez Melián <japmelian@gmail.com>
2013-10-13 12:02:21 +0100

    Avanzando en la suma

--- racional.rb
+++ racional.rb
@@ -4,12 +4,12 @@ require "./mcd.rb" # Máximo Común Divisor
 require "./mcm.rb" # Mínimo Común Múltiplo
 
 class Fraccion
-	
+
 	def initialize(*args)
 		if args.size == 2
-			c = mcd(args[0],args[1])
-			@a = (args[0]/c)
-			@b = (args[1]/c)
+			c = mcd(args[0], args[1])
+			@a = (args[0] / c)
+			@b = (args[1] / c)
 		else
 			@a = args[0]
 			@b = 1
@@ -20,20 +20,26 @@ class Fraccion
 		"#{@a}/#{@b}"
 	end
 
-	def suma (c, d)
+	def suma(c, d)
+		min = mcd(c, d)
+		c = (c / min)
+		d = (d / min)
 
+		@a += c
+		@b += d
 	end
 
-	def resta (c, d)
+	def resta(c, d)
 	end
 
-	def producto (c, d)
+	def producto(c, d)
 	end
 
-	def division (c, d)
+	def division(c, d)
 	end
 end
 
-A = Fraccion.new(2,4)
+A = Fraccion.new(2, 4)
+A.suma(2, 4)
 
 puts A
